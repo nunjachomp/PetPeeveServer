c181ec8852dbc95c6c241fa86305914a7b963356
nunjachomp <nunjachomp@gmail.com>
2023-09-03 18:54:20 +0300

    Added All Fields For Pet Searchbar

--- Migrations/20230828111409_pets.js
+++ Migrations/20230828111409_pets.js
@@ -3,17 +3,16 @@ exports.up = function(knex) {
         table.increments("id").primary();
         table.string('name').notNull();
         table.string("species").notNull();
-        // table.string("simple_classification").notNull();
-        // table.string('adoption_status').notNull();
-        // table.string('photo_url').notNull();
-        // table.float('height').notNull();
-        // table.float('weight').notNull();
-        // table.string('primary_color').notNull();
-        // table.string('bio').notNull();
-        // table.boolean('hypoallergenic').notNull();
-        // table.string('dietary_restrictions').notNull();
+        table.string("simple_classification").notNull();
+        table.string('adoption_status').notNull();
+        table.float('height').notNull();
+        table.float('weight').notNull();
+        table.string('primary_color').notNull();
+        table.string('bio').notNull();
+        table.boolean('hypoallergenic').notNull();
+        table.string('dietary_restrictions').notNull();
         table.timestamp("createdDate").defaultTo(knex.fn.now());
-        table.string("userId").notNull(); //why do I need this?
+        table.string("userId").notNull();
         table.string("petImage");
       })
     };
